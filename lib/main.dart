3518c5c20f20c775f567f2ff236725840d1a4c61
eimroda <adormie@outlook.com>
2023-08-19 15:54:41 +0800

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,165 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:lesson01/register.dart';
+import 'package:http/http.dart' as http;
+import 'package:lesson01/mainpage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  String url = 'http://localhost/contacts/api/user.php';
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  Future<void> login() async {
+    Map<String, String> jsonData = {
+      'username': _usernameController.text,
+      'password': _passwordController.text
+    };
+    Map<String, String> formData = {
+      'json': jsonEncode(jsonData),
+      'operation': 'login'
+    };
+
+    var response = await http.post(
+      Uri.parse(url),
+      body: formData,
+    );
+    print(response.body);
+    if (response.body != '0') {
+      Navigator.of(context).push(
+        MaterialPageRoute(builder: (context) => Mainpage()),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Invalid Username or password. Please try again!"),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Buck Fan Club',
+              style: TextStyle(fontSize: 20, color: Colors.blue),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            ClipOval(
+              child: Image.asset(
+                'assets/pictures/buck.jpg',
+                width: 250,
+                height: 150,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            const Text(
+              'Login',
+              style: TextStyle(fontSize: 20, color: Colors.black),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            TextFormField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                labelText: "Username",
+                hintText: "Inpur username",
+                prefixIcon: Icon(Icons.person),
+                suffixIcon: Icon(Icons.check),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0)),
+                labelText: "Password",
+                hintText: "Enter your password",
+                prefixIcon: Icon(Icons.password),
+              ),
+            ),
+            const SizedBox(height: 10),
+            SizedBox(
+              height: 40.0,
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  login();
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+                child: const Text("Login"),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Register()),
+                  );
+                },
+                child: const Text(
+                  "Click Here to Register",
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    decoration: TextDecoration.underline,
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/register.dart
@@ -0,0 +1,127 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Register extends StatefulWidget {
+  const Register({Key? key}) : super(key: key);
+
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  String url = "http://localhost/contacts/api/user.php";
+  TextEditingController _fullNameCntrl = TextEditingController();
+  TextEditingController _usernameCntrl = TextEditingController();
+  TextEditingController _passwordCntrl = TextEditingController();
+
+  Future<void> register() async {
+    Map<String, String> _jsonData = {
+      'fullname': _fullNameCntrl.text,
+      'username': _usernameCntrl.text,
+      'password': _passwordCntrl.text
+    };
+
+    Map<String, String> requestData = {
+      'json': jsonEncode(_jsonData),
+      'operation': 'signup'
+    };
+
+    var response = await http.post(Uri.parse(url), body: requestData);
+    print(response.body);
+    if (response.body == '1') {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("You have registered successfully!"),
+        ),
+      );
+      Navigator.pop(context);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Something is wrong."),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Register"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Buck Fan Club Registration",
+              style: TextStyle(color: Colors.red, fontSize: 20),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              controller: _fullNameCntrl,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0)),
+                labelText: "Full Name",
+                hintText: "Enter your full name",
+                prefixIcon: Icon(Icons.abc_outlined),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              controller: _usernameCntrl,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0)),
+                labelText: "Username",
+                hintText: "Enter your username",
+                prefixIcon: Icon(Icons.person_2),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              obscureText: true,
+              controller: _passwordCntrl,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0)),
+                labelText: "Password",
+                hintText: "Enter your password",
+                prefixIcon: Icon(Icons.password),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              height: 40,
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  register();
+                },
+                child: Text("Register"),
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
